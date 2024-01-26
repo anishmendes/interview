78b8a4d5732329f9abef784ecc585838757811fe
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-01-26 10:14:44 +0545

    Add files via upload

---
+++ src/components/Timer.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+
+ const timer {
+    
+ }
+const handleInputChange = (e) => {
+  setTimer({ ...Data, [e.target.name]: e.target.value });
+};
+
+
+const submitHandler = async event => {
+    event.preventDefault();
+    setLoading(true)
+
+const Timer = () => {
+ 
+  
+    <div className="time">
+    {Timer.map(time, timelist =>
+      <div className="time">
+        {child}
+      </div>
+    )}
+
+    />
+
+
+ 
+  
+}
+export default Timer
+
